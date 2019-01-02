f9053b1fdb1ae07cf583b2d2a50ebde89a37e6be
risheric <risheric@gmail.com>
2019-01-01 21:02:58 -0300

    first commit

---
+++ variables.py
@@ -0,0 +1,16 @@
+# saludo = "Hola "
+# nombre = input("Ingrese su nombre: ")
+
+# print(saludo + nombre)
+
+'''
+Comentario de multiples lineas
+'''
+
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
